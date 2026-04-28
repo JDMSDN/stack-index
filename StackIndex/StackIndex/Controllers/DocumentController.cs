1fdc5d38c2279dd2d6fd346ff2c3a5fb725cace4
Jevy Miranda <jevymira@outlook.com>
2026-04-27 17:03:24 -0700

    feat: add endpoint to get presigned S3 URL

--- StackIndex/StackIndex/Controllers/DocumentController.cs
+++ StackIndex/StackIndex/Controllers/DocumentController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Amazon.S3;
+using Amazon.S3.Model;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
 
 namespace StackIndex.Controllers;
 
@@ -6,9 +9,29 @@ namespace StackIndex.Controllers;
 [Route("[controller]")]
 public class DocumentController : ControllerBase
 {
-    [HttpGet()]
-    public ActionResult<string> GetPresignedUrlAsync()
+    [HttpGet("url")]
+    public Results<Ok<string>, InternalServerError> GetPresignedUrlAsync(IAmazonS3 client)
     {
-        return "";
+        string url = string.Empty;
+
+        try
+        {
+            var request = new GetPreSignedUrlRequest
+            {
+                BucketName = "stack-index",
+                Key = "sample.txt",
+                Verb = HttpVerb.PUT,
+                Expires = DateTime.UtcNow.AddHours(1)
+            };
+
+            url = client.GetPreSignedURL(request);
+        }
+        catch (AmazonS3Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+            return TypedResults.InternalServerError();
+        }
+
+        return TypedResults.Ok(url);
     }
 }
