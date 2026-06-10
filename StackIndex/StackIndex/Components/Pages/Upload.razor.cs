a7b754f1492481a29180ec2bb7f523fd8523833c
Jevy Miranda <jevymira@outlook.com>
2026-06-09 21:09:15 -0500

    feat: upload to S3 via presigned URL

---
+++ StackIndex/StackIndex/Components/Pages/Upload.razor.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using System.Net.Http.Headers;
+
+namespace StackIndex.Components.Pages;
+
+public partial class Upload
+{
+    [Inject]
+    private HttpClient HttpClient { get; set; } = default!;
+
+    private async Task UploadFile(InputFileChangeEventArgs e)
+    {
+        var file = e.File;
+        if (file is null) return;
+
+        var presignedUrl = await HttpClient.GetFromJsonAsync<string>($"Document/url");
+
+        await using var fileStream = file.OpenReadStream();
+
+        using var content = new StreamContent(fileStream);
+
+        content.Headers.ContentLength = fileStream.Length;
+
+        content.Headers.ContentType = new MediaTypeHeaderValue(file.ContentType);
+
+        var response = await HttpClient.PutAsync(presignedUrl, content);
+    }
+}
